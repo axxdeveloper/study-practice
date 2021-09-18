492b26e06b5fca6c005b9c7f887218ea843b695c
Isaac Liao <isaac.liao@commscope.com>
2021-09-19 01:03:35 +0800

    Add unit test to show original situation

--- src/test/java/examples/AnyTest.java
+++ src/test/java/examples/AnyTest.java
@@ -11,6 +11,16 @@ public class AnyTest {
     @Test
     public void testCreateRequest() throws InvalidProtocolBufferException {
         Request.CreateRequest createRequest = Request.CreateRequest.newBuilder().setName("name").build();
+        byte[] ary = createRequest.toByteArray();
+
+        // send to remove server, remote server need to parse the byte array
+        Request.CreateRequest receive = Request.CreateRequest.parseFrom(ary);
+        Assert.assertEquals(receive.getName(), createRequest.getName());
+    }
+
+    @Test
+    public void testAnyCreateRequest() throws InvalidProtocolBufferException {
+        Request.CreateRequest createRequest = Request.CreateRequest.newBuilder().setName("name").build();
         byte[] ary = Any.pack(createRequest).toByteArray();
         
         // send to remove server, remote server need to parse the byte array 
