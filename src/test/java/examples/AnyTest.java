f16322b3b84f6cd574a38c2d4f091cfd5d14c1d4
Isaac Liao <shooeugenesea.tw@gmail.com>
2021-09-19 00:18:52 +0800

    1. An example to generate java from proto 2. An unit test to show an Any example

---
+++ src/test/java/examples/AnyTest.java
@@ -0,0 +1,23 @@
+package examples;
+
+import com.google.protobuf.Any;
+import com.google.protobuf.InvalidProtocolBufferException;
+import examples.protobuf.Request;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AnyTest {
+
+    @Test
+    public void testCreateRequest() throws InvalidProtocolBufferException {
+        Request.CreateRequest createRequest = Request.CreateRequest.newBuilder().setName("name").build();
+        byte[] ary = Any.pack(createRequest).toByteArray();
+        
+        // send to remove server, remote server need to parse the byte array 
+        Any any = Any.parseFrom(ary);
+        Assert.assertTrue(any.is(Request.CreateRequest.class));
+        Request.CreateRequest unpack = any.unpack(Request.CreateRequest.class);
+        Assert.assertEquals(unpack.getName(), createRequest.getName());
+    }
+
+}
