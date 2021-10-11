341ec525a790292297b9a7484eca07a455a5f32a
Isaac Liao <shooeugenesea.tw@gmail.com>
2021-10-12 02:22:19 +0800

    Separate Kafka tests to different classes

--- src/test/java/com/shooeugenesea/kafka/KafkaTransactionalBeginTransactionTwiceTest.java
+++ src/test/java/com/shooeugenesea/kafka/KafkaTransactionalBeginTransactionTwiceTest.java
@@ -1,7 +1,6 @@
 package com.shooeugenesea.kafka;
 
 import com.shooeugenesea.IntegrationTest;
-import com.shooeugenesea.config.KafkaTopicConfig;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
@@ -18,35 +17,36 @@ import java.time.Duration;
 import java.util.Arrays;
 import java.util.Properties;
 import java.util.UUID;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 import static com.shooeugenesea.config.KafkaTopicConfig.TX_REQ_TOPIC_1;
 
 @SpringBootTest
-public class KafkaTransactionalTest extends IntegrationTest {
+public class KafkaTransactionalBeginTransactionTwiceTest extends IntegrationTest {
     
+    private String GROUP_ID = UUID.randomUUID().toString();
     
-//    @Test
-    public void test_begineTransactionTwice() {
+    @Test
+    public void test() {
         try (
             KafkaProducer<String, String> producer1 = producer(UUID.randomUUID().toString(), "tx-");
             KafkaProducer<String, String> producer2 = producer(UUID.randomUUID().toString(), "tx-");
-            KafkaConsumer<String, String> consumer = consumer_read_committed(UUID.randomUUID().toString(), TX_REQ_TOPIC_1)) {
+            KafkaConsumer<String, String> consumer = consumer_read_committed(TX_REQ_TOPIC_1)) {
             producer1.beginTransaction();
             
             producer2.beginTransaction();
             String msg1 = UUID.randomUUID().toString();
             producer1.send(new ProducerRecord<>(TX_REQ_TOPIC_1, msg1));
                         
-            String msg2 = UUID.randomUUID().toString();
-            producer2.send(new ProducerRecord<>(TX_REQ_TOPIC_1, msg2));
             
             try {
                 producer1.commitTransaction();
                 Assert.fail("producer1 is too old to commit");
-            } catch (Exception ex) {}
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
             
+            String msg2 = UUID.randomUUID().toString();
+            producer2.send(new ProducerRecord<>(TX_REQ_TOPIC_1, msg2));
             producer2.commitTransaction();
             
             ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(10));
@@ -56,51 +56,6 @@ public class KafkaTransactionalTest extends IntegrationTest {
                 Assert.assertEquals(msg2, r.value());
             });
         } 
-    }
-
-//    @Test
-    public void test_readUncommitted() {
-        try (
-            KafkaProducer<String, String> producer = producer(UUID.randomUUID().toString(), "tx-" + UUID.randomUUID().toString());
-            KafkaConsumer<String, String> consumer = consumer_read_uncommitted(UUID.randomUUID().toString(), TX_REQ_TOPIC_1)) {
-            producer.beginTransaction();
-            String msg = UUID.randomUUID().toString();
-            producer.send(new ProducerRecord<>(TX_REQ_TOPIC_1, msg));
-            
-            ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(10));
-            Assert.assertEquals(1, records.count());
-            records.forEach(r -> {
-                System.out.println("Before commit: " + r.value() + " sent:" + msg);
-                Assert.assertEquals(msg, r.value());
-            });
-            
-            producer.commitTransaction();
-            
-            records = consumer.poll(Duration.ofSeconds(10));
-            Assert.assertEquals(0, records.count());
-            records.forEach(r -> System.out.println("After commit: " + r.value() + ", sent:" + msg));
-        }
-    }
-
-    @Test
-    public void test_readCommitted() {
-        try (
-            KafkaProducer<String, String> producer = producer(UUID.randomUUID().toString(), "tx-" + UUID.randomUUID().toString());
-            KafkaConsumer<String, String> consumer = consumer_read_committed(UUID.randomUUID().toString(), TX_REQ_TOPIC_1)) {
-            producer.beginTransaction();
-            String msg = UUID.randomUUID().toString();
-            producer.send(new ProducerRecord<>(TX_REQ_TOPIC_1, msg));
-            
-            ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(10));
-            Assert.assertEquals(0, records.count());
-            records.forEach(r -> System.out.println("Before commit: " + r.value() + " sent:" + msg));
-            
-            producer.commitTransaction();
-            
-            records = consumer.poll(Duration.ofSeconds(10));
-            Assert.assertEquals(1, records.count());
-            records.forEach(r -> System.out.println("After commit: " + r.value() + ", sent:" + msg));
-        }
     }
 
     private KafkaProducer<String, String> producer(String clientId, String txId) {
@@ -116,31 +71,32 @@ public class KafkaTransactionalTest extends IntegrationTest {
         return producer;
     }
 
-    private KafkaConsumer<String, String> consumer_read_uncommitted(String groupId, String...topics) {
+    private KafkaConsumer<String, String> consumer_read_uncommitted(String...topics) {
         final Properties props = new Properties();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaContainer.getBootstrapServers());
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
         props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
+        props.put(ConsumerConfig.ISOLATION_LEVEL_CONFIG, "read_uncommitted"); // let consumer only consume message which is committed
         
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
         
         final KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
         consumer.subscribe(Arrays.asList(topics));
         return consumer;
     }
 
-    private KafkaConsumer<String, String> consumer_read_committed(String groupId, String...topics) {
+    private KafkaConsumer<String, String> consumer_read_committed(String...topics) {
         final Properties props = new Properties();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaContainer.getBootstrapServers());
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
         props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
         props.put(ConsumerConfig.ISOLATION_LEVEL_CONFIG, "read_committed"); // let consumer only consume message which is committed
         
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
         
         final KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
         consumer.subscribe(Arrays.asList(topics));
