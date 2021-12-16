1cef0e9da4829a2899a0275fb3d4a9680cda808a
Isaac Liao <isaac.liao@ruckuswireless.com>
2021-12-16 14:50:57 +0800

    not work

--- src/test/java/examples/HelloTest.java
+++ src/test/java/examples/HelloTest.java
@@ -1,12 +1,12 @@
 package examples;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-
-import org.junit.Assert;
-import org.junit.Test;
 
 public class HelloTest {
 
@@ -15,7 +15,7 @@ public class HelloTest {
         Hello h = new Hello();
         String hello = h.hello();
         String hellotest = new String(Files.readAllBytes(Paths.get(Thread.currentThread().getContextClassLoader().getResource("hellotest.txt").toURI())));
-        Assert.assertEquals(hellotest, hello);
+        Assertions.assertEquals(hellotest, hello);
     }
     
 }
